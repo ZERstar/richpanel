0bee327970357151301985edd4b6b609d2e471d2
Wade <hhh@example.com>
2023-08-06 17:29:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import './App.css'; // Import the custom CSS file
+import Home from './Home';
+import Login from './login';
+import Register from './register';
+import PlanDetails from './PlanDetails';
+import PaymentForm from './PaymentForm';
+import MyPlan from './MyPlan';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div classname='dd' style={{display : 'flex'}}>
+
+        {/* Use the <Routes> component to wrap multiple <Route> components */}
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/PlanDetails" element={<PlanDetails />} />
+          <Route path="/PaymentForm" element={<PaymentForm />} />
+          <Route path="/MyPlan" element={<MyPlan />} />
+
+          {/* You can add more routes here */}
+          {/* <Route path="/some-other-page" element={<SomeOtherComponent />} /> */}
+          {/* <Route path="/yet-another-page" element={<YetAnotherComponent />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
